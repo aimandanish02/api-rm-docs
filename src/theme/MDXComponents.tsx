cbdba91de0645cb41100aa12dc202cdf5252987c
Aiman Danish <aimandanish955@gmail.com>
2026-04-28 10:16:16 +0800

    feat: add MarkdownTable to MDXComponents for enhanced table rendering

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -1,11 +1,13 @@
 import ApiPlayground from "../components/ApiPlayground";
 import ApiExamples from "../components/ApiExamples";
 import ParamTable from "../components/ParamTable";
-import CodeBlock from "../components/CodeBlock"; 
+import CodeBlock from "../components/CodeBlock";
+import MarkdownTable from "../components/MarkdownTable";
 
 export default {
   ApiPlayground,
   ApiExamples,
   ParamTable,
   CodeBlock,
+  table: MarkdownTable,
 };
